2590f04615b462c3007043b3e6452af6de5412b4
LucasRafaelBersotLinares <lucasblinares@gmail.com>
2026-03-23 22:15:00 -0300

    Termino exercicios aula7

--- Exercicios_Aulas/src/aula7.ts
+++ Exercicios_Aulas/src/aula7.ts
@@ -19,11 +19,31 @@ nome.forEach((item) => {
 */
 
 /*Exercício 3:
-function transforma(array:string[]) {
-    return array.sort((a,b) => a.localeCompare(b, undefined, {numeric: true}))
+function transforma(array:string[]):string[] {
+    let p = array.slice();
+    return p.sort((a,b) => a.localeCompare(b, undefined, {numeric: true}))
 }
 const array: string[] = ['1.10.0', '1.2.1', '1.0.5', '1.20.0'];
-console.log(transforma(array));
+const arrayT: string[] = transforma(array);
+console.log(arrayT);
+console.log(array)
+*/
+
+/*Exercício 4:
+const estudantes = [
+    {nome: 'Ricardo', nota: 8.5, bolsista: false},
+    {nome: 'Maria', nota: 7.5, bolsista: true},
+    {nome: 'Talio', nota: 6.5, bolsista: false},
+    {nome: 'Falcon', nota: 4.5, bolsista: true},
+    {nome: 'Anisio', nota: 1.5, bolsista: false}
+];
+
+let estudantesBonus = estudantes.map(n => n.nota * 0.5);
+console.log(estudantesBonus);
+console.log(estudantesBonus.sort());
+estudantes.forEach((item) => {
+    console.log(`${item.nome} e sua nota:${item.nota}`)
+})
 */
 
 
