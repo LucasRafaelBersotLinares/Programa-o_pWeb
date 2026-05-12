403875d8779674a46d75af68d93b126ef2279b2d
LucasRafaelBersotLinares <lucasblinares@gmail.com>
2026-05-11 23:10:43 -0300

    Exercicio 14 feito!

--- Exercicios_Aulas/src/exercicio_14/exercicio_4/classes/pessoa.ts
+++ Exercicios_Aulas/src/exercicio_14/exercicio_4/classes/pessoa.ts
@@ -44,18 +44,47 @@ export class Pessoa {
         }
     }
     validarDataNascimento(): void {
-        let anoColocado = this.dataNascimento.slice(6,this.dataNascimento.length)
-        let dateAtual = new Date((this.dataNascimento))
-        let dateColocada = new Date(this.dataNascimento)
-        dateColocada.setDate(dateColocada.getDate()+1)
-        let anoAtual = dateAtual.toLocaleDateString().slice(6,dateAtual.toLocaleDateString().length)
-        console.log(dateColocada)
         try{
-            if(this.dataNascimento){}
-
+            if(!this.dataNascimento.includes("/")){
+                throw new Error("Data de nascimento nao chegue o formato dd/mm/yyyy")
+            }
+            for (let i = 1; i < 4; i++) {
+                if(i == 1){
+                    let firstValidation = this.dataNascimento.slice(0,2)
+                    let validationBar = this.dataNascimento.slice(2,3)
+                    if(validationBar.includes("/")){
+                        if(Number(firstValidation) > 0 && Number(firstValidation) <= 31){
+                            continue
+                        }
+                        throw new Error("Data de nascimento nao chegue o formato dd/mm/yyyy")
+                        break
+                    }
+                }
+                if(i == 2){
+                    let secondValidation = this.dataNascimento.slice(3,5)
+                    let validationBar = this.dataNascimento.slice(5,6)
+                    if(validationBar.includes("/")){
+                        if(Number(secondValidation) > 0 && Number(secondValidation) <= 31){
+                            continue
+                        }
+                        throw new Error("Data de nascimento nao chegue o formato dd/mm/yyyy")
+                        break
+                    }
+                }
+                if(i == 3){
+                    let treeValidation = this.dataNascimento.slice(6,10)
+                    if(Number(treeValidation) > 1927 && Number(treeValidation) <= 2026){
+                        continue
+                    }
+                    throw new Error("Data de nascimento nao chegue o formato dd/mm/yyyy ou sua Idade esta acima de 99 anos")
+                    break
+                    }
+            }
+            console.log("Data de nascimento validada com sucesso")
+            return
         }catch(erro){
             if(erro instanceof Error){
-                console.log("Data de nascimento nao esta no formato: dd/mm/yyyy")
+                console.log(erro.message)
             }
         }
     }
--- Exercicios_Aulas/src/exercicio_14/exercicio_4/main.ts
+++ Exercicios_Aulas/src/exercicio_14/exercicio_4/main.ts
@@ -1,4 +1,6 @@
 import {Pessoa} from "./classes/pessoa"
 
-let object = new Pessoa("lucas", "lucas@gmail.com", "12/08/2000")
+let object = new Pessoa("LUCAS RAFAEL BERSOT LINARES", "lucasblinares@gmail.com", "22/07/2006")
 object.validarDataNascimento()
+object.validarNome()
+object.validarEmail()
